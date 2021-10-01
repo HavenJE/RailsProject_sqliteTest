8897777d2ae56bd33dba850f264bfa8a995269fa
haven <haven.eldebaque@gmail.com>
2021-10-01 14:31:07 +1000

    initial style pushed to sessions_exercises_controllers

--- db/schema.rb
+++ db/schema.rb
@@ -10,27 +10,28 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_09_28_041932) do
+ActiveRecord::Schema.define(version: 2021_09_30_061449) do
 
-  create_table "comments", force: :cascade do |t|
-    t.string "content"
-    t.integer "user_id", null: false
-    t.integer "posts_id", null: false
+  create_table "categories", force: :cascade do |t|
+    t.text "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
-    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
-  create_table "posts", force: :cascade do |t|
-    t.string "title"
-    t.text "content"
+  create_table "exercises", force: :cascade do |t|
+    t.text "title"
+    t.text "description"
     t.integer "user_id", null: false
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["category_id"], name: "index_posts_on_category_id"
-    t.index ["user_id"], name: "index_posts_on_user_id"
+    t.index ["category_id"], name: "index_exercises_on_category_id"
+    t.index ["user_id"], name: "index_exercises_on_user_id"
+  end
+
+  create_table "sessions", force: :cascade do |t|
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", force: :cascade do |t|
@@ -41,8 +42,6 @@ ActiveRecord::Schema.define(version: 2021_09_28_041932) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
-  add_foreign_key "comments", "posts", column: "posts_id"
-  add_foreign_key "comments", "users"
-  add_foreign_key "posts", "categories"
-  add_foreign_key "posts", "users"
+  add_foreign_key "exercises", "categories"
+  add_foreign_key "exercises", "users"
 end
